7f14c80eda261d353fd1a0a3fee2e7d24f42b5cc
pert <petr777@yahoo.com>
2022-11-02 02:15:51 +0300

    add state and catch DB exeptions

--- src/flows/etl.py
+++ src/flows/etl.py
@@ -1,83 +1,104 @@
 import asyncio
-import sqlalchemy as sa
-from prefect import flow
+from prefect import flow, task, get_run_logger
 from prefect.task_runners import ConcurrentTaskRunner
 from schemas import ConvertSchema
 from database import session_scope
 from models import MediaOriginalModel, MediaConvertModel
-from aiofiles import os
 from extract import extracted_task
 from convert import converted_task
 from upload import upload_task
-from schemas import ETLSchema, MediaInfoDB
+from schemas import ETLSchema
 from services.media_info import MediaInfoServices
 from settings import settings
+from prefect.futures import PrefectFuture
 
 
-async def file_video_encoding(item: MediaInfoDB, batch_size: int, etl_schema: ETLSchema):
-
-    temp_file = await extracted_task(str(item.filepath), item.bucket)
-
-    convert_file = await converted_task(temp_file, etl_schema.convert_schema)
-
-    upload_file = await upload_task(
-        convert_file.name, etl_schema.bucket_out, etl_schema.destination_folder
-    )
-
-    info = MediaInfoServices(convert_file.name)
-
-    movie_info = MediaConvertModel(
-        bucket=etl_schema.bucket_out,
-        filepath=upload_file,
-        movies_id=item.movies_id,
-        original_id=item.id,
-        hash_convert=etl_schema.convert_schema.get_hash(),
-        **info.short_info.dict()
-    )
-
-    async with session_scope() as session:
-        session.add(movie_info)
-        await session.commit()
-        await session.refresh(movie_info)
-
-    await os.remove(temp_file.name)
-    await os.remove(convert_file.name)
+async def get_state_task(task_future: PrefectFuture):
+    if await task_future.wait():
+        state = await task_future.get_state()
+        if state.is_completed():
+            return 'Success'
+        return 'Error'
 
 
-async def get_original_not_have_convert_schema(
-        convert_schema: ConvertSchema,
-        batch_size: int
-):
-    hash_convert = convert_schema.get_hash()
+async def video_encoding_proccess(item, etl_schema: ETLSchema):
 
-    subquery = sa.select(MediaConvertModel.original_id).filter(
-        MediaConvertModel.hash_convert == hash_convert)
+    temp_file_future = await extracted_task.submit(item.filepath, item.bucket)
+    if await get_state_task(temp_file_future) == 'Success':
+        result = await temp_file_future.result()
+        convert_file_future = await converted_task.submit(
+            result, etl_schema.convert_schema
+        )
+        if await get_state_task(convert_file_future) == 'Success':
 
-    query = sa.select(
-        MediaOriginalModel).where(MediaOriginalModel.id.notin_(subquery))
+            result = await convert_file_future.result()
+            info = MediaInfoServices(result.name)
 
-    async with session_scope() as session:
-        result = await session.execute(query)
-        result = result.scalars()
-        data = [
-            MediaInfoDB(**row.as_dict())
-            for row in result.all()
-        ]
-        return data[:batch_size]
+            upload_future = await upload_task.submit(
+                result.name,
+                etl_schema.bucket_out,
+                etl_schema.destination_folder
+            )
+
+            if await get_state_task(upload_future) == 'Success':
+
+                async with session_scope() as session:
+                    convert_model = MediaConvertModel()
+                    await convert_model.add_info(
+                        session,
+                        filepath=await upload_future.result(),
+                        bucket=etl_schema.bucket_out,
+                        movies_id=item.movies_id,
+                        original_id=item.id,
+                        hash_convert=etl_schema.convert_schema.get_hash(),
+                        media_info=info.short_info
+                    )
+                return upload_future.result()
 
 
-@flow(task_runner=ConcurrentTaskRunner(), name="start_video_encoding")
-async def video_encoding(etl_schema: ETLSchema):
-    convert_schema = etl_schema.convert_schema
-    batch_size = settings.batch_size
-
-    tasks = await get_original_not_have_convert_schema(convert_schema, batch_size)
-
+@flow(task_runner=ConcurrentTaskRunner(), name="video_encoding")
+async def sub_flow(tasks, etl_schema: ETLSchema):
     task_video_encoding = [
-        file_video_encoding(item, batch_size, etl_schema=etl_schema) for item in tasks
+        video_encoding_proccess(item, etl_schema=etl_schema) for item in tasks
     ]
     await asyncio.gather(*task_video_encoding)
 
 
+@task(retries=3, retry_delay_seconds=10)
+async def get_original_not_have_convert_schema(
+        convert_schema: ConvertSchema,
+        batch_size: int
+):
+    async with session_scope() as session:
+        original_model = MediaOriginalModel()
+        tasks = await original_model.get_original_not_have_convert_schema(
+            session, convert_schema
+        )
+        return tasks[:batch_size]
+
+
+@flow(task_runner=ConcurrentTaskRunner(), name="start_video_encoding")
+async def video_encoding(etl_schema: ETLSchema):
+    logger = get_run_logger()
+    convert_schema = etl_schema.convert_schema
+    batch_size = settings.batch_size
+
+    future = await get_original_not_have_convert_schema.submit(
+        convert_schema, batch_size
+    )
+    video_encoding_task = await future.result()
+
+    if video_encoding_task:
+        await sub_flow(video_encoding_task, etl_schema)
+
+    logger.info('There are no conversion tasks for the specified parameters')
+
+#
 # loop = asyncio.new_event_loop()
-# loop.run_until_complete(video_encoding({'destination_folder': 'rutube', 'convert_schema': {"size": "1920x1080", "vcodec": "libx264", 'acodec': "mp3"}}))
+# loop.run_until_complete(video_encoding(
+#     {
+#         'destination_folder': 'medium',
+#         'convert_schema': {
+#             "size": "1280x720", "vcodec": "libx264", "acodec": "aac"}
+#     }
+# ))
--- src/models.py
+++ src/models.py
@@ -1,10 +1,22 @@
+import enum
 import uuid
 import sqlalchemy as sa
 from sqlalchemy.orm import declarative_mixin, relationship
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.dialects.postgresql import UUID
 from database import Base
 from datetime import datetime
+from schemas import ConvertSchema
+from prefect import get_run_logger
+from schemas import MediaInfoSchema
 
+
+class ObjectDoesNotExistError(Exception):
+    """Raise it if object does not exist in database."""
+
+
+class ObjectAlreadyExistError(Exception):
+    """Raise it if object already exist in database."""
 
 @declarative_mixin
 class TimestampMixin:
@@ -50,6 +62,32 @@ class MediaOriginalModel(Base, TimestampMixin, BaseMediaMixin):
     )
     movies = relationship("MoviesModel", foreign_keys=[movies_id])
 
+    @classmethod
+    async def _get_obj(cls, db_session, stmt: sa.sql.Select):
+        result = await db_session.execute(stmt)
+        obj = result.scalar()
+        if not obj:
+            raise ObjectDoesNotExistError
+        return obj
+
+    @classmethod
+    async def get_original_not_have_convert_schema(
+            cls,
+            db_session: AsyncSession,
+            convert_schema: ConvertSchema
+    ):
+        logger = get_run_logger()
+        hash_convert = convert_schema.get_hash()
+        subquery = sa.select(MediaConvertModel.original_id).filter(
+            MediaConvertModel.hash_convert == hash_convert)
+        query = sa.select(
+            MediaOriginalModel).where(MediaOriginalModel.id.notin_(subquery))
+        try:
+            result = await db_session.execute(query)
+            return result.scalars().all()
+        except Exception as error:
+            logger.error(f'DB Error {error}')
+
     def as_dict(self):
         return {
             c.key: getattr(self, c.key)
@@ -60,6 +98,7 @@ class MediaOriginalModel(Base, TimestampMixin, BaseMediaMixin):
 class MediaConvertModel(Base, TimestampMixin, BaseMediaMixin):
 
     __tablename__ = "convert_media"
+
 
     hash_convert = sa.Column(
         sa.String(length=128), nullable=False, unique=False
@@ -76,6 +115,40 @@ class MediaConvertModel(Base, TimestampMixin, BaseMediaMixin):
     media_original = relationship(
         "MediaOriginalModel", foreign_keys=[original_id]
     )
+
+    @classmethod
+    async def _get_obj(cls, db_session, stmt: sa.sql.Select):
+        result = await db_session.execute(stmt)
+        obj = result.scalar()
+        if not obj:
+            raise ObjectDoesNotExistError
+        return obj
+
+    @classmethod
+    async def add_info(
+            cls,
+            db_session: AsyncSession,
+            filepath: str,
+            bucket: str,
+            hash_convert: str,
+            movies_id: str,
+            original_id: str,
+            media_info: MediaInfoSchema,
+    ):
+        media_convert = MediaConvertModel(
+            filepath=filepath,
+            bucket=bucket,
+            movies_id=movies_id,
+            original_id=original_id,
+            hash_convert=hash_convert,
+            **media_info.dict()
+        )
+        db_session.add(media_convert)
+        try:
+            await db_session.commit()
+        except sa.exc.IntegrityError:
+            raise ObjectAlreadyExistError
+        await db_session.refresh(media_convert)
 
     def as_dict(self):
         return {
